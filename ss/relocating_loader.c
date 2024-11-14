ed764f8830858091b1d9cb32cbd02a71407ba748
roshy1234 <jamesroshy@gmail.com>
2024-11-14 11:20:29 +0530

    loader update

--- ss/relocating_loader.c
+++ ss/relocating_loader.c
@@ -1,88 +1,104 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
-void main() {
-    FILE *input;
-    input = fopen("object.txt", "r");
-
-    if (input == NULL) {
-        printf("Error: Could not open file.\n");
-        return 1;
-    }
-
-    char rec, name[10], mask[10], bit[13], code[10], addr[10];
-    int st, add, txtLen, pgrAdd, pgrLen, address, len;
-
-    // Reading first character to identify record type
-    fscanf(input, "%c", &rec);
-    printf("Enter the starting address : ");
-    scanf("%x", &st);
-
-    while (rec != 'E') {
-        if (rec == 'H') {
-            // Handling Header Record
-            fscanf(input, "%s %x %x", name, &pgrAdd, &pgrLen);
-            printf("Name of program : %s\n", name);
-            printf("Length of program : %X\n", pgrLen);
-            printf("----------------------------\n");
-            printf("ADDRESS\t\tCONTENT\n");
-            printf("----------------------------\n");
-        } 
-        else if (rec == 'T') {
-            // Handling Text Record
-            fscanf(input, "%x %x %s %s %s", &address, &txtLen, mask, code, addr);
-
-            // Convert mask (hexadecimal) to binary and store in bit[]
-            strcpy(bit, "");
-            int num = 0;
-            for (int i = 0; i < strlen(mask); i++) {
-                int n = mask[i];
-                num = num * 16 + (n < 58 ? n - 48 : n - 55);
-            }
-            while (num != 0) {
-                if (num % 2 == 0) {
-                    strcat(bit, "0");
-                } else {
-                    strcat(bit, "1");
-                }
-                num /= 2;
-            }
-            // Reverse the bit array to maintain correct order
-            int i = 0, j = strlen(bit) - 1;
-            while (i < j) {
-                char temp = bit[i];
-                bit[i] = bit[j];
-                bit[j] = temp;
-                i++;
-                j--;
-            }
-
-            int index = 0;
-            address += st;  // Adjust start address for relocation
-
-            while (strcmp(code, "-") != 0) {
-                if (bit[index] == '1') {
-                    // If relocation bit is 1, add start address to relocate
-                    int addrInt = 0;
-                    for (int k = 0; k < strlen(addr); k++) {
-                        int n = addr[k];
-                        addrInt = addrInt * 16 + (n < 58 ? n - 48 : n - 55);
-                    }
-                    add = addrInt + st;
-                    printf("%X\t\t%s%X\n", address, code, add);
-                } else {
-                    printf("%X\t\t%s%s\n", address, code, addr);
-                }
-                address += 3;  // Move to the next address (assuming 3 bytes per code entry)
-                index++;
-                fscanf(input, "%s %s", code, addr);
-            }
-        }
-
-        // Reading next record type
-        fscanf(input, "%c", &rec);
-    }
-
-    fclose(input);
-}
+#include <stdlib.h>
+void convert(char h[12]);
+char bitmask[12];
+char bit[12] = {0};
+void main()
+{
+ char add[6], length[10], input[10], binary[12], relocbit, ch, pn[5];
+ int start, inp, len, i, address, opcode, addr, actualadd, tlen;
+ FILE *fp1, *fp2;
+ printf("\n\n Enter the actual starting address : ");
+ scanf("%x", &start);
+ fp1 = fopen("inputr.txt", "r");
+ fp2 = fopen("output.txt", "w");
+ fscanf(fp1, "%s", input);
+ fprintf(fp2, " ----------------------------\n");
+ fprintf(fp2, " ADDRESS\tCONTENT\n");
+ fprintf(fp2, " ----------------------------\n");
+ while (strcmp(input, "E") != 0)
+ {
+ if (strcmp(input, "H") == 0)
+ {
+ fscanf(fp1, "%s", pn);
+ fscanf(fp1, "%x", add);
+ fscanf(fp1, "%x", length);
+ fscanf(fp1, "%s", input);
+ }
+ if (strcmp(input, "T") == 0)
+ {
+ fscanf(fp1, "%x", &address);
+ fscanf(fp1, "%x", &tlen);
+ fscanf(fp1, "%s", bitmask);
+ address += start;
+ convert(bitmask);
+ len = strlen(bit);
+ if (len >= 11)
+ len = 10;
+ for (i = 0; i < len; i++)
+ {
+ fscanf(fp1, "%x", &opcode);
+ fscanf(fp1, "%x", &addr);
+ relocbit = bit[i];
+ if (relocbit == '0')
+ actualadd = addr;
+ else
+ actualadd = addr + start;
+ fprintf(fp2, "\n %x\t\t%x%x\n", address, opcode, actualadd);
+ address += 3; }
+ fscanf(fp1, "%s", input);}
+ }
+ fprintf(fp2, " ----------------------------\n");
+ fclose(fp1);
+ fclose(fp2);
+ printf("\n\n The contents of output file(output.txt) \n\n");
+ fp2 = fopen("output.txt", "r");
+ ch = fgetc(fp2);
+ while (ch != EOF)
+ {
+ printf("%c", ch); 
+  ch = fgetc(fp2); }
+ fclose(fp2);
+}void convert(char h[12])
+{ int i, l;
+ strcpy(bit, "");
+ l = strlen(h);
+ for (i = 0; i < l; i++)
+ {switch (h[i])
+ {
+ case '0':
+ strcat(bit, "0"); break;
+ case '1':
+ strcat(bit, "1"); break;
+ case '2':
+ strcat(bit, "10");break;
+ case '3':
+ strcat(bit, "11"); break;
+ case '4':
+ strcat(bit, "100");break;
+ case '5':
+ strcat(bit, "101"); break;
+ case '6':
+ strcat(bit, "110");break;
+ case '7':
+ strcat(bit, "111");break;
+ case '8':
+ strcat(bit, "1000"); break;
+ case '9':
+ strcat(bit, "1001"); break;
+ case 'A':
+ strcat(bit, "1010"); break;
+ case 'B':
+ strcat(bit, "1011");break;
+ case 'C':
+ strcat(bit, "1100");break;
+ case 'D':
+ strcat(bit, "1101"); break;
+ case 'E':
+ strcat(bit, "1110");break;
+ case 'F':
+ strcat(bit, "1111");break;
+ }
+ }
+} 
